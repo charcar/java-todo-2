ba6a692d2282541fa4656454f544addd630a43c8
Charles Carter <charles.h.carter@gmail.com>
2016-02-18 11:57:21 -0800

    we did everything.

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import org.junit.Rule;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.fluentlenium.adapter.FluentTest;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @Rule
+  public ClearRule clearRule = new ClearRule();
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Todo list!");
+  }
+
+  @Test
+  public void categoryIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a new category"));
+    fill("#name").with("Household chores");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your category has been saved.");
+  }
+
+  @Test
+  public void categoryIsDisplayedTest() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Household chores");
+    submit(".btn");
+    click("a", withText("View Categories"));
+    assertThat(pageSource()).contains("Household chores");
+  }
+
+  @Test
+  public void categoryTasksFormIsDisplayed() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Shopping");
+    submit(".btn");
+    click("a", withText("View Categories"));
+    click("a", withText("Shopping"));
+    click("a", withText("Add a new task"));
+    assertThat(pageSource()).contains("Add a Task to Shopping");
+  }
+
+  @Test
+  public void tasksIsAddedAndDisplayed() {
+    goTo("http://localhost:4567/categories/new");
+    fill(#name).with("Playing");
+    submit(".btn");
+    click("a", withText("View Categories"));
+    click("a", withText("Playing"));
+    click("a", withText("Add a new task"));
+    fill("#description").with("Existential questioning");
+    submit(".btn");
+    assertThat(pagesSource()).contains("Existential questioning");
+  }
+
+
+
+
+}
